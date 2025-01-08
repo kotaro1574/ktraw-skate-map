b56eb4814384e8e5055d32d0705ba278c3036480
kotaro1574 <shake1574@gmail.com>
2025-01-08 12:57:58 +0900

    update: mapのセンターをバンクーバーに

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ const youTubeContents = [
 export default function IndexPage() {
   return (
     <section className="">
-      <Map center={[51.505, -0.09]} zoom={13} />
+      <Map center={[49.2827, -123.1207]} zoom={13} />
       <div className="container py-8">
         <h1 className="mt-8 text-center text-4xl font-bold">最新の投稿</h1>
         <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
