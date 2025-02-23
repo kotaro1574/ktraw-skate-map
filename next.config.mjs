2f25272e2987f06e34779edc83ef1451644afc48
kotaro1574 <shake1574@gmail.com>
2025-02-23 10:40:59 +0900

    update: `next.config.js`にnext-pwaモジュールの設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import nextPWA from "next-pwa"
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})
+
+const nextConfig = withPWA({
   reactStrictMode: true,
-}
+})
 
 export default nextConfig
