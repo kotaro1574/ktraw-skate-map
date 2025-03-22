641bab4c3ccc4971237af4c0724d89adc22596c2
kotaro1574 <shake1574@gmail.com>
2025-03-22 10:48:43 +0900

    fix: データ復元

--- config/areas-data.ts
+++ config/areas-data.ts
@@ -22,46 +22,50 @@ export const areasData: Area[] = [
       lng: -123.1207,
     },
   },
-  // {
-  //   id: 2,
-  //   name: "バーナビー",
-  //   nameEn: "burnaby",
-  //   countryId: 1,
-  //   center: {
-  //     lat: 49.2487,
-  //     lng: -122.9787,
-  //   },
-  // },
-  // {
-  //   id: 3,
-  //   name: "リッチモンド",
-  //   nameEn: "richmond",
-  //   countryId: 1,
-  //   center: {
-  //     lat: 49.1666,
-  //     lng: -123.1336,
-  //   },
-  // },
-  // {
-  //   id: 4,
-  //   name: "ポートランド",
-  //   nameEn: "portland",
-  //   countryId: 2,
-  //   center: {
-  //     lat: 45.5051,
-  //     lng: -122.675,
-  //   },
-  // },
-  // {
-  //   id: 5,
-  //   name: "シアトル",
-  //   nameEn: "seattle",
-  //   countryId: 2,
-  //   center: {
-  //     lat: 47.6062,
-  //     lng: -122.3321,
-  //   },
-  // },
+  {
+    id: 2,
+    name: "バーナビー",
+    nameEn: "burnaby",
+    countryId: 1,
+    zoom: 11,
+    center: {
+      lat: 49.2487,
+      lng: -122.9787,
+    },
+  },
+  {
+    id: 3,
+    name: "リッチモンド",
+    nameEn: "richmond",
+    countryId: 1,
+    zoom: 11,
+    center: {
+      lat: 49.1666,
+      lng: -123.1336,
+    },
+  },
+  {
+    id: 4,
+    name: "ポートランド",
+    nameEn: "portland",
+    countryId: 2,
+    zoom: 11,
+    center: {
+      lat: 45.5051,
+      lng: -122.675,
+    },
+  },
+  {
+    id: 5,
+    name: "シアトル",
+    nameEn: "seattle",
+    countryId: 2,
+    zoom: 11,
+    center: {
+      lat: 47.6062,
+      lng: -122.3321,
+    },
+  },
   {
     id: 6,
     name: "岡山",
