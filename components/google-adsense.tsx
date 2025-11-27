5d3781c10f697799cccb8f0c3d9cb145095e74d1
kotaro1574 <shake1574@gmail.com>
2025-11-27 11:03:23 +0900

    fix

--- components/google-adsense.tsx
+++ components/google-adsense.tsx
@@ -5,9 +5,9 @@ type Props = {
 }
 
 const GoogleAdsense: React.FC<Props> = ({ pId }) => {
-  if (process.env.NODE_ENV !== "production") {
-    return null
-  }
+  // if (process.env.NODE_ENV !== "production") {
+  //   return null
+  // }
   return (
     <Script
       async
